f1635f495d888f6f0f41b882e2e8c282f2e28e45
lvpeng990324 <lvpeng990324@gmail.com>
2021-11-13 10:01:35 +0800

    标记已读接口增加total参数，给了就标记该用户所有未读为已读

--- Chat/apis/MarkAsRead.py
+++ Chat/apis/MarkAsRead.py
@@ -16,15 +16,16 @@ from utils.logger import logger
 
 class SingleChatMarkAsRead(View):
     """ 单聊标记为已读
-    待确认：如果一个人的一个会话未读消息太多了，他只加载了最近的几条，那么之前的那些未读消息要不要改已读，
-          但是，如果只标记了部分已读的话，那么就会出现点击进入一个会话页面再退出的话，这个会话列表项还会有未读的红点提示
-          先这么做吧，如果后面有问题要改动，再考虑其他办法
-    目前是会将这个会话中接收方是当前用户的所有消息全标记为已读
+    将这个会话中接收方是当前用户的所有消息全标记为已读
+    如果给了total，就把当前用户的所有未读标记为已读
     """
     @method_decorator(check_user_sig)
     def post(self, request):
         # 获取session_id
         session_id = request.POST.get('session_id')
+        # 获取是否标记该用户所有未读消息已读的flag
+        total = request.POST.get('total')  # 给了total就是直接标记此人的所有未读消息，上方的session_id就不会用到了
+
         # 从headers中获取user_id
         user_id = get_user_id_from_headers(headers=request.headers)
         # 取出该用户
@@ -37,35 +38,44 @@ class SingleChatMarkAsRead(View):
             )
         # 取到该用户了
 
-        # 取出这个用户参与的该session
-        try:
-            session = Session.objects.get(
-                id=session_id,  # 要这个id的session
-                type=SessionTypeOption.SINGLE,  # 要单聊的
-                chat_users__user_id__contains=user_id,  # 要这个用户参与的
+        # 判断是否给了total
+        if total == 'total':
+            # 给了total，标记该用户所有的未读为已读
+            # 将当前用户的所有未读消息标记为已读
+            to_mark_chat_logs = ChatLog.objects.filter(
+                have_read=False,  # 要未读的
+            ).exclude(
+                sender=chat_user,  # 不要发送者是当前用户的，也即接收者是当前用户的
             )
-        except Session.DoesNotExist:
-            # 未取到符合条件的，session不存在或者该用户未参与
-            return response_404(
-                message='session不存在或该用户未参与该会话',
-            )
-        # 取到session了
+        else:
+            # 没给total，正常标记给定的session的给用户的未读消息为已读
+            # 取出这个用户参与的该session
+            try:
+                session = Session.objects.get(
+                    id=session_id,  # 要这个id的session
+                    type=SessionTypeOption.SINGLE,  # 要单聊的
+                    chat_users__user_id__contains=user_id,  # 要这个用户参与的
+                )
+            except Session.DoesNotExist:
+                # 未取到符合条件的，session不存在或者该用户未参与
+                return response_404(
+                    message='session不存在或该用户未参与该会话',
+                )
+            # 取到session了
 
-        # 将这个session中当前用户的所有未读消息标记为已读
-        to_mark_chat_logs = ChatLog.objects.filter(
-            session=session,  # 要这个session的
-            have_read=False,  # 要未读的
-        ).exclude(
-            sender=chat_user,  # 不要发送者是当前用户的，也即接收者是当前用户的
-        )
-        # 标记为已读并记录已读时间
-        to_mark_chat_logs.update(
+            # 将这个session中当前用户的所有未读消息标记为已读
+            to_mark_chat_logs = ChatLog.objects.filter(
+                session=session,  # 要这个session的
+                have_read=False,  # 要未读的
+            ).exclude(
+                sender=chat_user,  # 不要发送者是当前用户的，也即接收者是当前用户的
+            )
+
+        # 标记为已读并记录已读时间，记录标记条数
+        marked_count = to_mark_chat_logs.update(
             have_read=True,  # 标记已读
             read_time=datetime.datetime.now(),  # 记录当前时间为已读时间
         )
-
-        # 记录已读条数
-        marked_count = to_mark_chat_logs.count()
 
         # 记录日志
         logger.info(f'{user_id} 已读 {session_id} 会话的 {marked_count} 条信息')
